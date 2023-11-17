d7ffd34f65ccd2ec180a12e6e08bc1a654cff12c
nicohash <58517269+nicohash@users.noreply.github.com>
2023-11-17 11:48:42 +0100

    refactor to individual workbooks

--- code/utils.py
+++ code/utils.py
@@ -1,13 +1,21 @@
 import numpy as np
+#import pandas as pd
 
-def encode_cyclical_feature(df, column, period):
+def encode_cyclical_feature(input_df, output_df, column, period):
     """ Encodes cyclical features to sine and cosine
 
     Args:
-        df (DataFrame): The input data frame
-        column (int64): The feature to encode
+        input_df (DataFrame): The input data frame
+        output_df (DataFrame): The output data frame
+        column (str): The feature to encode
         period (int64): The cycle duration
     """
-    df[column + '_sin'] = np.sin(2 * np.pi * df[column] / period)
-    df[column + '_cos'] = np.cos(2 * np.pi * df[column] / period)
-    df.drop(column, axis=1, inplace=True)
+    sin_values = np.sin(2 * np.pi * input_df[column] / period)
+    cos_values = np.cos(2 * np.pi * input_df[column] / period)
+
+    output_df[column + '_sin'] = sin_values
+    output_df[column + '_cos'] = cos_values
+    #position = df.columns.get_loc(column) + 1
+    #df.insert(position, column + '_sin', sin_values)
+    #df.insert(position + 1, column + '_cos', cos_values)
+    # df.drop(column, axis=1, inplace=True)
